961cc505c2c990815126e01814bbe6f9ce011924
hventayen <55516542+hventayen@users.noreply.github.com>
2022-09-20 23:04:26 -0700

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib
+
+
+# In[2]:
+
+
+import pandas
+
+
+# In[3]:
+
+
+import sklearn
+
+
+# In[4]:
+
+
+import scipy
+
+
+# In[5]:
+
+
+import numpy
+
+
+# In[6]:
+
+
+import keras
+
+
+# In[7]:
+
+
+import tensorflow
+
+
+# In[8]:
+
+
+get_ipython().system('python3 --version')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
